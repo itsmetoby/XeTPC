dd38be2e597c6b977a62f29a192c2d8e31ff9871
itsmetoby <111298473+itsmetoby@users.noreply.github.com>
2025-05-06 15:31:04 +0800

    Add files via upload
    To visualize electric fields inside the TPC

---
+++ E_fields/XeTPC_efield.C
@@ -0,0 +1,77 @@
+#include <TApplication.h>
+#include <TCanvas.h>
+#include <TH1F.h>
+#include <cmath>
+#include <cstring>
+
+#include <cstdlib>
+#include <iostream>
+
+#include "Garfield/AvalancheMC.hh"
+#include "Garfield/AvalancheMicroscopic.hh"
+#include "Garfield/ComponentAnsys123.hh"
+#include "Garfield/MediumMagboltz.hh"
+#include "Garfield/Random.hh"
+#include "Garfield/Sensor.hh"
+#include "Garfield/ViewDrift.hh"
+#include "Garfield/ViewFEMesh.hh"
+#include "Garfield/ViewField.hh"
+#include "Garfield/ComponentElmer.hh"
+#include "Garfield/ViewFEMesh.hh"
+
+
+
+using namespace Garfield;
+
+int main(int argc, char* argv[]) {
+  TApplication app("app", &argc, argv);
+
+  // Setup the gas.
+  MediumMagboltz gas("xe",100);
+  gas.SetTemperature(293.15);
+  gas.SetPressure(760.);
+  gas.Initialise(true);
+  gas.LoadGasFile("/Users/tobychan/garfield/Gasfile_codes/xe_100_500Vcm.gas");
+
+  // Import Elmer   
+  ComponentElmer * elm = new ComponentElmer("/Users/tobychan/garfield/result_file_3/mesh.header",
+    "/Users/tobychan/garfield/result_file_3/mesh.elements",
+    "/Users/tobychan/garfield/result_file_3/mesh.nodes",
+    "/Users/tobychan/garfield/result_file_3/dielectrics.dat",
+    "/Users/tobychan/garfield/result_file_3/v2a_shield_500V.result",
+    "cm"
+  );
+  elm->EnablePeriodicityX();
+  elm->EnableMirrorPeriodicityY();
+  elm->SetMedium(81,&gas);
+  elm->SetWeightingField("/Users/tobychan/garfield/result_file_3/v2a_shield_weight.result", "readout");
+
+  // extx=2,exty=2,extz=1
+  Sensor* sensor = new Sensor();
+  sensor->AddComponent(elm);
+  sensor->SetArea(-2, -2, -1, 2, 2, 1);
+
+
+  TCanvas* c1 = new TCanvas("c1","c1",800,800);
+  TCanvas* c2 = new TCanvas("c2","c2",800,800);
+
+  ViewField * vf = new ViewField();
+  vf->SetSensor(sensor);
+  vf->SetCanvas(c1);
+  vf->SetArea(-2, -0.1, 2, 1);
+  vf->SetNumberOfContours(20);
+  vf->SetNumberOfSamples2d(50,50);
+  vf->SetPlane(0,-1,0,0,0,0);
+  
+  ViewFEMesh * vFE = new ViewFEMesh();
+  vFE->SetCanvas(c1);
+  vFE->SetComponent(elm);
+  vFE->SetPlane(0,-0.1,0,0,0,0);
+  vFE->SetFillMesh(true);
+  vFE->SetColor(0,kBlue);
+  vFE->SetArea(-2, -2, -0.1, 2, 2, 1);
+  
+  vf->PlotContour("v");
+  c1->Draw();
+  c1->SaveAs("XeTPC_efield.pdf");
+}
