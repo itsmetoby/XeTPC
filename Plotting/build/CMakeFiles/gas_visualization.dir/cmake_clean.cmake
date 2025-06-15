6c181878c6e67bd7040a4dd1bf059041c6aafd9c
keiyinau <eltisau@outlook.com>
2025-06-15 09:06:16 +0800

    update

---
+++ Plotting/build/CMakeFiles/gas_visualization.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gas_visualization.dir/gas_visualization.C.o"
+  "CMakeFiles/gas_visualization.dir/gas_visualization.C.o.d"
+  "gas_visualization"
+  "gas_visualization.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gas_visualization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Plotting/gas_visualization.C
+++ Plotting/gas_visualization.C
@@ -14,31 +14,40 @@ int main(int argc, char* argv[]) {
   MediumMagboltz gas;
   gas.LoadGasFile("xe_500v.gas");
   gas.PrintGas();
+  std::cout << "Now read the gas medium"<< std::endl;
   ViewMedium view(&gas);
-
+  std::cout << "End of reading the gas medium"<< std::endl;
   TCanvas cV("cV", "", 600, 600);
   view.SetCanvas(&cV);
   view.PlotElectronVelocity('e'); 
-  app.Run(true);
+  cV.SaveAs("electron_velocity.png");
 
-  //TCanvas cD("cD", "", 600, 600);
-  //view.SetCanvas(&cD);
-  //view.PlotElectronDiffusion();
-  //app.Run(true);
-//
-  //TCanvas cT("cT", "", 600, 600);
-  //view.SetCanvas(&cT);
-  //view.PlotElectronTownsend();
-  //app.Run(true);
-//
-  //TCanvas cA("cA", "", 600, 600);
-  //view.SetCanvas(&cA);
-  //view.PlotElectronAttachment();
-  //app.Run(true);
-//
-  //TCanvas cI("cI", "", 600, 600);
-  //view.SetCanvas(&cI);
-  //view.PlotIonVelocity();
-  //app.Run(true);
+  TCanvas cD("cD", "", 600, 600);
+  view.SetCanvas(&cD);
+  view.PlotElectronDiffusion();
+  cD.SaveAs("electron_diffusion.png");
+
+  TCanvas cT("cT", "", 600, 600);
+  view.SetCanvas(&cT);
+  view.PlotElectronTownsend();
+  cT.SaveAs("electron_townsend.png");
+
+  TCanvas cA("cA", "", 600, 600);
+  view.SetCanvas(&cA);
+  view.PlotElectronAttachment();
+  cA.SaveAs("electron_attachment.png");
+
+  TCanvas cI("cI", "", 600, 600);
+  view.SetCanvas(&cI);
+  view.PlotIonVelocity();
+  cI.SaveAs("ion_velocity.png");
+  TCanvas cD2("cD2", "", 600, 600);
+  view.SetCanvas(&cD2);
+  view.PlotIonDiffusion();
+  cD2.SaveAs("ion_diffusion.png");
+
+
+  app.Run();
+  return 0;
 }
 
